a8c2cb3d6f70cd109a412ebf54ba492df9d5f74c
Oleksandr <kulibaba99@gmail.com>
2022-07-24 18:47:13 +0300

    layout

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+
+import './app-info.css'
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employees record</h1>
+            <h2>Total employees amount: </h2>
+            <h2>Employees to receive bonus: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
