9b6d74c39563a20644c62d961f4812cd6babffef
Oleksandr <kulibaba99@gmail.com>
2022-07-26 20:20:41 +0300

    added option for saving to favorites / highlighting workers to promotion. added methods for tracking form input

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,30 +1,59 @@
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => {
-
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase) {
-        classNames += ' increase';
+class EmployeesListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            favorite: false
+        }
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
+    onAddToFavorites = () => {
+        this.setState(({favorite}) => ({
+            favorite: !favorite
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {increase, favorite} = this.state;
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (increase) {
+            classNames += ' increase';
+        }
+
+        if (favorite) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span onClick={this.onAddToFavorites} className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
     )
+    }
 }
 
 export default EmployeesListItem;
